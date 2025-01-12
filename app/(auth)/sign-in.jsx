2b1c8dd25223c1593946b8a19893b94566c63cc8
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-01-13 00:27:44 +1100

    add authentication screens and update layout; implement sign-in and sign-up forms; enhance form field component with password visibility toggle

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,12 +3,20 @@ import React,{ useState }  from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
+import CustomBotton from '../../components/CustomButton'
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: '',
     password: ''
   });
+
+  const [isSubmitting, setisSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -34,6 +42,14 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles='mt-7'
           />
+          <CustomBotton title='Sign In' handlePress={submit}
+          containerStyles='mt-7' isLoading={isSubmitting}/>
+          <View className='justify-center pt-5 flex-row font-pregular'>
+            <Text className='text-lg text-gray'>
+              Don't have an account?
+            </Text>
+            <Link href='/sign-up' className="text-lg font-psmibold text-secondary ps-1">Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,6 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
+import { icons } from "../constants";
 
 const FormField = ({
   title,
@@ -11,20 +12,29 @@ const FormField = ({
 }) => {
   const [showPassword, setshowPassword] = useState(false);
   return (
-    <View className={`space-y-2 ${otherStyles}`}>
-      <Text className="text-base text-gray font-pmedium">{title}</Text>
+    <View className={` ${otherStyles}`}>
+      <Text className="text-base text-gray font-pmedium ms-2 mb-2">{title}</Text>
       <View
-        className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl
-      focus:border-secondary " 
+        className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center flex-row"
       >
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
           value={value}
           placeholder={placeholder}
-          placeholderTextColor='#7b7b8b'
+          placeholderTextColor="#7b7b8b"
           onChangeText={handleChangeText}
-          secureTextEntry={(title === "Password") && !showPassword}
+          secureTextEntry={title === "Password" && !showPassword}
         />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setshowPassword(!showPassword)}>
+            <Image
+              source={showPassword ? icons.eyehide : icons.eye}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
